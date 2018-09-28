53bedfff88b92dda80c665dd79f11cfe750bf494
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-09-28 09:40:36 +0300

    Format types

--- library/Explicit/Classes/Types.hs
+++ library/Explicit/Classes/Types.hs
@@ -18,13 +18,21 @@ newtype Functor m = Functor (forall a b. (a -> b) -> (m a -> m b))
 
 newtype Pointed m = Pointed (forall a. a -> m a)
 
-data Apply m = Apply (Functor m) (forall a b. m (a -> b) -> (m a -> m b)) (forall a b. m a -> m b -> m b) (forall a b. m a -> m b -> m a) (forall a b c. (a -> b -> c) -> m a -> m b -> m c)
+data Apply m = Apply
+  (Functor m)
+  (forall a b. m (a -> b) -> (m a -> m b))
+  (forall a b. m a -> m b -> m b)
+  (forall a b. m a -> m b -> m a)
+  (forall a b c. (a -> b -> c) -> m a -> m b -> m c)
 
 data Applicative m = Applicative (Pointed m) (Apply m)
 
 newtype Alt m = Alt (forall a. m a -> m a -> m a)
 
-data Bind m = Bind (Apply m) (forall a. m (m a) -> m a) (forall a b. m a -> (a -> m b) -> m b)
+data Bind m = Bind
+  (Apply m)
+  (forall a. m (m a) -> m a)
+  (forall a b. m a -> (a -> m b) -> m b)
 
 data Monad m = Monad (Applicative m) (Bind m)
 
@@ -32,11 +40,19 @@ data Monad m = Monad (Applicative m) (Bind m)
 -- * Contravariant functors
 -------------------------
 
-data Contravariant m = Contravariant (forall a b. (a -> b) -> m b -> m a) (forall a b. b -> m b -> m a)
+data Contravariant m = Contravariant
+  (forall a b. (a -> b) -> m b -> m a)
+  (forall a b. b -> m b -> m a)
 
-data Divisible m = Divisible (Contravariant m) (forall a b c. (a -> (b, c)) -> m b -> m c -> m a) (forall a. m a)
+data Divisible m = Divisible
+  (Contravariant m)
+  (forall a b c. (a -> (b, c)) -> m b -> m c -> m a)
+  (forall a. m a)
 
-data Decidable m = Decidable (Divisible m) (forall a. (a -> Void) -> m a) (forall a b c. (a -> Either b c) -> m b -> m c -> m a)
+data Decidable m = Decidable
+  (Divisible m)
+  (forall a. (a -> Void) -> m a)
+  (forall a b c. (a -> Either b c) -> m b -> m c -> m a)
 
 
 -- * Comonads
@@ -44,6 +60,9 @@ data Decidable m = Decidable (Divisible m) (forall a. (a -> Void) -> m a) (foral
 
 newtype Copointed m = Copointed (forall a. m a -> a)
 
-data Extend m = Extend (Functor m) (forall a. m a -> m (m a)) (forall a b. (m a -> b) -> m a -> m b)
+data Extend m = Extend
+  (Functor m)
+  (forall a. m a -> m (m a))
+  (forall a b. (m a -> b) -> m a -> m b)
 
 data Comonad m = Comonad (Copointed m) (Extend m)
